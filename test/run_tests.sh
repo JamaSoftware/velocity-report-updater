73b2270bdd970f8808a9adb074b657a49bb79744
Nick Burns <nburns@jamasoftware.com>
2019-10-10 10:11:20 -0700

    better test output

--- test/run_tests.sh
+++ test/run_tests.sh
@@ -4,6 +4,8 @@ cd "$BASEDIR" || exit 1
 
 RED='\033[0;31m'
 GREEN='\033[0;32m'
+BLUE='\033[0;34m'
+NORMAL='\033[0m'
 
 
 # make a temp dir and copy the input file to it
@@ -14,11 +16,15 @@ cp ./input.vm "$TEMPDIR"/file_mutated_by_tests.vm
 yes | ../report-updater.sh "$TEMPDIR"
 
 # check if the output matches what we expected
-if diff --side-by-side "$TEMPDIR"/file_mutated_by_tests.vm ./expected_output.vm; then
+INPUT="$TEMPDIR"/file_mutated_by_tests.vm
+OUTPUT=./expected_output.vm
+
+echo $BLUE"INPUT:                                                          OUTPUT:"$NORMAL
+if diff --side-by-side "$INPUT" "$OUTPUT" ; then
     rm -rf "$TEMPDIR"
-    echo $GREEN"passed"
+    echo $GREEN"TESTS PASS"
 else
-    #rm -rf "$TEMPDIR"
-    echo $RED"failed"
+    rm -rf "$TEMPDIR"
+    echo $RED"TESTS FAIL"
     exit 1
 fi
